2db93dc3ebb57ee61a613b43623976a197ae5bd9
Mohammad Khalid Hasan <rabbikhalidhasan@gmail.com>
2023-07-10 23:29:17 +0200

    HamBurger menu implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import NavBar from "@/scenes/navbar";
+import { useState, useEffect } from "react";
+import { SelectedPage } from "./shared/type";
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => removeEventListener("scroll", handleScroll);
+  }, []);
   return (
     <>
       <div className="app bg-gray-20">
-        <NavBar />
+        <NavBar
+          isTopOfPage={isTopOfPage}
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+        />
       </div>
     </>
   );
