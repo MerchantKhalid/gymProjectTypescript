a3a7f605f2f3d572ad06ff52f31bb1d66898e38d
Mohammad Khalid Hasan <rabbikhalidhasan@gmail.com>
2023-07-10 15:39:45 +0200

    Navbar added

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import NavBar from "@/scenes/navbar";
+
+function App() {
+  return (
+    <>
+      <div className="app bg-gray-20">
+        <NavBar />
+      </div>
+    </>
+  );
+}
+
+export default App;
