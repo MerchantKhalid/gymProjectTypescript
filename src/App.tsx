f0b57f658f7ce415c528d5ece084d4403641c82d
Mohammad Khalid Hasan <rabbikhalidhasan@gmail.com>
2023-07-12 16:10:34 +0200

    framer motion added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import NavBar from "@/scenes/navbar";
+import Home from "@/scenes/home";
 import { useState, useEffect } from "react";
 import { SelectedPage } from "./shared/type";
 
@@ -7,6 +8,18 @@ function App() {
     SelectedPage.Home
   );
   const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     if (window.scrollY === 0) {
+  //       setIsTopOfPage(true);
+  //       setSelectedPage(SelectedPage.Home);
+  //     }
+  //     if (window.scrollY !== 0) setIsTopOfPage(false);
+  //   };
+  //   window.addEventListener("scroll", handleScroll);
+  //   return () => removeEventListener("scroll", handleScroll);
+  // }, []);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -17,18 +30,18 @@ function App() {
       if (window.scrollY !== 0) setIsTopOfPage(false);
     };
     window.addEventListener("scroll", handleScroll);
-    return () => removeEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
+
   return (
-    <>
-      <div className="app bg-gray-20">
-        <NavBar
-          isTopOfPage={isTopOfPage}
-          selectedPage={selectedPage}
-          setSelectedPage={setSelectedPage}
-        />
-      </div>
-    </>
+    <div className="app bg-gray-20">
+      <NavBar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <Home />
+    </div>
   );
 }
 
